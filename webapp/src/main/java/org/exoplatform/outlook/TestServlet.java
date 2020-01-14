e349cca1b7119aa41869b1025c85753660e9b711
Nick Riabovol <nick.riabovol@gmail.com>
2020-01-14 17:09:02 +0200

    Added and configured TestServlet

---
+++ webapp/src/main/java/org/exoplatform/outlook/TestServlet.java
@@ -0,0 +1,203 @@
+/*
+ * Copyright (C) 2003-2009 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.exoplatform.outlook;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+import org.exoplatform.commons.utils.ListAccess;
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.container.component.RequestLifeCycle;
+import org.exoplatform.container.web.AbstractHttpServlet;
+import org.exoplatform.outlook.portlet.IdentityInfo;
+import org.exoplatform.outlook.security.OutlookTokenService;
+import org.exoplatform.outlook.web.RequestUtils;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.services.organization.OrganizationService;
+import org.exoplatform.services.organization.User;
+import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
+import org.exoplatform.social.core.manager.IdentityManager;
+import org.exoplatform.web.login.LoginServlet;
+import org.exoplatform.web.login.LogoutControl;
+import org.exoplatform.web.security.GateInToken;
+import org.exoplatform.web.security.security.AbstractTokenService;
+import org.exoplatform.web.security.security.CookieTokenService;
+
+
+/**
+ * The servlet.
+ */
+public class TestServlet extends AbstractHttpServlet  {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -1330051083735349589L;
+
+    /** . */
+    private static final Log log = ExoLogger.getLogger(TestServlet.class);
+
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.warn("[SampleServlet]: Current portal " + getContainer());
+        log.warn("[SampleServlet]: Servlet Context " + getServletContext());
+
+
+        List<IdentityInfo> list = null;
+        try {
+            list = findUsersByEmail("nik.riabovol@gmail.com", null);
+        } catch (Exception e) {
+            log.error("findUsersByEmail Exception", e);
+        }
+
+        if(list!=null){
+            log.warn("[SampleServlet]: list length: " + list.size());
+
+            try {
+                log.warn("[SampleServlet]: getFullName " + list.toArray(new IdentityInfo[0])[0].getFullName());
+                resp.getWriter().print(list.toArray(new IdentityInfo[0])[0].getFullName());
+            } catch (Exception e) {
+                log.warn("[SampleServlet]: getFullName error ",e);
+            }
+        } else {
+            log.warn("[SampleServlet]: list is null ");
+        }
+
+
+
+        try {
+            // We set the character encoding now to UTF-8 before obtaining parameters
+            req.setCharacterEncoding("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            log.error("Encoding not supported", e);
+        }
+
+        log.warn("Test servlet doGet");
+
+        try {
+            log.warn("fullLogout start");
+            fullLogout(req,resp);
+            log.warn("fullLogout finish");
+        } catch (Exception e) {
+            log.error("fullLogout Exception", e);
+        }
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    private List<IdentityInfo> findUsersByEmail(String emails, String excludeOne) throws Exception {
+        List<IdentityInfo> list = new ArrayList<>();
+        ExoContainer currentContainer = ExoContainerContext.getCurrentContainer();
+        RequestLifeCycle.begin(currentContainer);
+        try {
+            for (String email : emails.split(",")) {
+                User user = findUserByEmail(email.toLowerCase());
+                if (user != null) {
+                    IdentityManager identityManager =
+                            (IdentityManager) currentContainer
+                                    .getComponentInstance(IdentityManager.class);
+                    Identity userIdentity = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME, user.getUserName(), false);
+                    if (userIdentity != null && (excludeOne != null ? !excludeOne.equals(userIdentity.getRemoteId()) : true)) {
+                        list.add(new IdentityInfo(userIdentity));
+                    }
+                }
+            }
+        } catch (Exception exception) {
+            log.warn("Error while findUsersByEmail: " + emails , exception);
+        } finally {
+            RequestLifeCycle.end();
+        }
+
+        return list;
+    }
+
+    private User findUserByEmail(String email) throws Exception {
+        ExoContainer currentContainer = ExoContainerContext.getCurrentContainer();
+        org.exoplatform.services.organization.Query query = new org.exoplatform.services.organization.Query();
+        query.setEmail(email);
+        OrganizationService organization =
+                (OrganizationService) currentContainer
+                        .getComponentInstance(OrganizationService.class);
+        ListAccess<User> res = organization.getUserHandler()
+                .findUsersByQuery(query, org.exoplatform.services.organization.UserStatus.ENABLED);
+        if (res.getSize() > 0) {
+            return res.load(0, 1)[0];
+        }
+        return null;
+    }
+
+    void fullLogout(HttpServletRequest req, HttpServletResponse res) {
+        // XXX repeating logic of UIPortal.LogoutActionListener
+    /*PortalRequestContext prContext = Util.getPortalRequestContext();
+    HttpServletRequest req = prContext.getRequest();
+    HttpServletResponse res = prContext.getResponse();*/
+
+        // Delete the token from JCR
+        String token = RequestUtils.getCookie(req, LoginServlet.COOKIE_NAME); // getTokenCookie(req)
+        if (token != null) {
+            AbstractTokenService<GateInToken, String> tokenService = AbstractTokenService.getInstance(CookieTokenService.class);
+            tokenService.deleteToken(token);
+        }
+        token = LoginServlet.getOauthRememberMeTokenCookie(req);
+        if (token != null) {
+            AbstractTokenService<GateInToken, String> tokenService = AbstractTokenService.getInstance(CookieTokenService.class);
+            tokenService.deleteToken(token);
+        }
+
+        LogoutControl.wantLogout();
+        Cookie cookie = new Cookie(LoginServlet.COOKIE_NAME, "");
+        cookie.setPath(req.getContextPath());
+        cookie.setMaxAge(0);
+        res.addCookie(cookie);
+
+        Cookie oauthCookie = new Cookie(LoginServlet.OAUTH_COOKIE_NAME, "");
+        oauthCookie.setPath(req.getContextPath());
+        oauthCookie.setMaxAge(0);
+        res.addCookie(oauthCookie);
+
+        // **********
+        // Outlook add-in logout (cookies)
+        String rememberMeOutlook = RequestUtils.getCookie(req, OutlookTokenService.COOKIE_NAME);
+        if (rememberMeOutlook != null) {
+            OutlookTokenService outlookTokens = AbstractTokenService.getInstance(OutlookTokenService.class);
+            outlookTokens.deleteToken(rememberMeOutlook);
+        }
+        Cookie rememberMeOutlookCookie = new Cookie(OutlookTokenService.COOKIE_NAME, "");
+        rememberMeOutlookCookie.setPath(req.getRequestURI());
+        rememberMeOutlookCookie.setMaxAge(0);
+        res.addCookie(rememberMeOutlookCookie);
+    }
+}
