4178ccf8e989d9bee6ee4c0e74e766fc8c0a5ccb
Aleksandra Strelkova <aleksandrageor8@gmail.com>
2020-02-13 18:45:35 +0200

    fixed project running, added components

---
+++ webapp/src/main/webapp/react-app/containers/ConvertToActivity/convert-to-activity.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './convert-to-activity.less';
+
+class ConvertToActivity extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>ConvertToActivity</h1>
+            </div>
+        );
+    }
+}
+
+export default ConvertToActivity;
