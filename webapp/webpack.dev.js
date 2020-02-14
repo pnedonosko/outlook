f281cbd63f9054cb2d8f892965b86d901ccf052e
Aleksandra Strelkova <aleksandrageor8@gmail.com>
2020-02-14 13:02:43 +0200

    removed unnecessary comments

--- webapp/webpack.dev.js
+++ webapp/webpack.dev.js
@@ -7,8 +7,6 @@ const app = "outlook";
 
 // add the server path to your server location path
 const exoServerPath = process.env.EXO_OUTLOOK_PLATFORM;
-// const exoServerPath = "/eXo/outlook/platform-6.0.0-M16";
-// const exoServerPath = 'D:/sasha_work/2020-02/platform3/platform-6.0.0-M16';
 
 let config = merge(webpackCommonConfig, {
   output: {
