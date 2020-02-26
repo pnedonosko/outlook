9ecdd556f646aa1b3bee63afd407355689961abb
Nick Riabovol <nick.riabovol@gmail.com>
2020-02-26 12:25:48 +0200

    Changed the rest.dto.Space structure (Space is a wrapper over outlook.OutlookSpace)

--- webapp/src/main/java/org/exoplatform/outlook/app/rest/dto/Space.java
+++ webapp/src/main/java/org/exoplatform/outlook/app/rest/dto/Space.java
@@ -1,46 +1,28 @@
 package org.exoplatform.outlook.app.rest.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.exoplatform.outlook.OutlookException;
 import org.exoplatform.outlook.OutlookSpace;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.jcr.RepositoryException;
+import java.util.Objects;
 
 /**
  * The type Space.
  */
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class Space {
 
   /** The Constant LOG. */
-  private static final Log LOG = ExoLogger.getLogger(Space.class);
+  private static final Log     LOG = ExoLogger.getLogger(Space.class);
 
   /**
-   * The Group id.
+   * The Outlook space.
    */
-  @JsonProperty("groupId")
-  String                   groupId;
-
-  /**
-   * The Title.
-   */
-  @JsonProperty("title")
-  String                   title;
-
-  /**
-   * The Root path.
-   */
-  @JsonProperty("rootPath")
-  String                   rootPath;
-
-  /**
-   * The Default folder path.
-   */
-  @JsonProperty("defaultFolderPath")
-  String                   defaultFolderPath;
+  protected final OutlookSpace outlookSpace;
 
   /**
    * Instantiates a new Outlook space.
@@ -48,22 +30,85 @@ public class Space {
    * @param outlookSpace the outlook space
    */
   public Space(OutlookSpace outlookSpace) {
-    this.groupId = outlookSpace.getGroupId();
-    this.title = outlookSpace.getTitle();
-    try {
-      this.rootPath = outlookSpace.getRootFolder().getPath();
-    } catch (OutlookException e) {
-      LOG.error(e);
-    } catch (RepositoryException e) {
-      LOG.error(e);
-    }
+    this.outlookSpace = outlookSpace;
+  }
 
+  /**
+   * Gets group id.
+   *
+   * @return the group id
+   */
+  @JsonProperty("groupId")
+  public String getGroupId() {
+    return outlookSpace.getGroupId();
+  }
+
+  /**
+   * Gets title.
+   *
+   * @return the title
+   */
+  @JsonProperty("title")
+  public String getTitle() {
+    return outlookSpace.getTitle();
+  }
+
+  /**
+   * Gets root path.
+   *
+   * @return the root path
+   */
+  @JsonProperty("rootPath")
+  public String getRootPath() {
+    String rootPath = "";
     try {
-      this.defaultFolderPath = outlookSpace.getRootFolder().getDefaultSubfolder().getPath();
+      rootPath = outlookSpace.getRootFolder().getPath();
     } catch (OutlookException e) {
-      LOG.error(e);
+      LOG.error("Error getting a space root folder in order to get the root path (rootPath)", e);
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                                        "Error getting a space root folder in order to get the root path (rootPath)");
     } catch (RepositoryException e) {
-      LOG.error(e);
+      LOG.error("Error getting a space root folder in order to get the root path (rootPath)", e);
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                                        "Error getting a space root folder in order to get the root path (rootPath)");
     }
+    return rootPath;
+  }
+
+  /**
+   * Gets default folder path.
+   *
+   * @return the default folder path
+   */
+  @JsonProperty("defaultFolderPath")
+  public String getDefaultFolderPath() {
+    String defaultFolderPath = "";
+    try {
+      defaultFolderPath = outlookSpace.getRootFolder().getDefaultSubfolder().getPath();
+    } catch (OutlookException e) {
+      LOG.error("Error getting a space root folder in order to get the default subfolder path (defaultFolderPath)", e);
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                                        "Error getting a space root folder in order to get the default subfolder path (defaultFolderPath)");
+    } catch (RepositoryException e) {
+      LOG.error("Error getting a space root folder in order to get the default subfolder path (defaultFolderPath)", e);
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                                        "Error getting a space root folder in order to get the default subfolder path (defaultFolderPath)");
+    }
+    return defaultFolderPath;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    Space space = (Space) o;
+    return Objects.equals(outlookSpace, space.outlookSpace);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(outlookSpace);
   }
 }
