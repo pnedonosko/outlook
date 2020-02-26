e12bddd21b92e3e3b00f34864d464a5a18056751
Nick Riabovol <nick.riabovol@gmail.com>
2020-02-26 23:16:14 +0200

    Added constants for Outlook (OutlookConstant)

---
+++ services/src/main/java/org/exoplatform/outlook/model/OutlookConstant.java
@@ -0,0 +1,33 @@
+package org.exoplatform.outlook.model;
+
+/**
+ * The type Outlook constant.
+ */
+public class OutlookConstant {
+
+  /**
+   * The constant HAL_AND_JSON.
+   */
+  public static final String HAL_AND_JSON  = "application/hal+json";
+
+  /**
+   * The Constant USER_ID.
+   */
+  public static final String USER_ID       = "$UID";
+
+  /**
+   * The Constant ACTIVITY_ID.
+   */
+  public static final String ACTIVITY_ID   = "$AID";
+
+  /**
+   * The Constant ACTIVITY_ID.
+   */
+  public static final String SPACE_ID      = "$SID";
+
+  /**
+   * The constant DOCUMENT_PATH.
+   */
+  public static final String DOCUMENT_PATH = "$DPATH";
+
+}
