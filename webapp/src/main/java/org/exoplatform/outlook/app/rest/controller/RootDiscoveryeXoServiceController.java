9203b4531421609f6fcad65b51bad755b5c4c3b8
Nick Riabovol <nick.riabovol@gmail.com>
2020-02-27 13:02:21 +0200

    Added RootDiscoveryeXoServiceController and stubs for  needed requests

---
+++ webapp/src/main/java/org/exoplatform/outlook/app/rest/controller/RootDiscoveryeXoServiceController.java
@@ -0,0 +1,29 @@
+package org.exoplatform.outlook.app.rest.controller;
+
+import org.exoplatform.outlook.app.rest.dto.AbstractFileResource;
+import org.exoplatform.outlook.model.OutlookConstant;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The type Root discovery eXo service controller.
+ */
+@RestController
+@RequestMapping(value = "/v2/exo")
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+public class RootDiscoveryeXoServiceController {
+
+  /**
+   * Gets root discovery of outlook mail services.
+   *
+   * @return the root discovery of outlook mail services
+   */
+  @RequestMapping(value = "", method = RequestMethod.GET, produces = OutlookConstant.HAL_AND_JSON)
+  public AbstractFileResource getRootDiscoveryOfOutlookMailServices() {
+    AbstractFileResource resource = null;
+
+    return resource;
+  }
+}
