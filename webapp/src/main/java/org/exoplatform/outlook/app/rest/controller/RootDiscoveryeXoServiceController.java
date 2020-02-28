4c943e1dd8b87cbba37ec4fad5139c37b0a5416f
Nick Riabovol <nick.riabovol@gmail.com>
2020-02-29 01:28:38 +0200

    Implemented RootDiscoveryeXoServiceController GET '/v2/exo' request handling

--- webapp/src/main/java/org/exoplatform/outlook/app/rest/controller/RootDiscoveryeXoServiceController.java
+++ webapp/src/main/java/org/exoplatform/outlook/app/rest/controller/RootDiscoveryeXoServiceController.java
@@ -1,11 +1,18 @@
 package org.exoplatform.outlook.app.rest.controller;
 
-import org.exoplatform.outlook.app.rest.dto.AbstractFileResource;
 import org.exoplatform.outlook.model.OutlookConstant;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.ResourceSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.springframework.hateoas.core.DummyInvocationUtils.methodOn;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 /**
  * The type Root discovery eXo service controller.
@@ -21,8 +28,17 @@ public class RootDiscoveryeXoServiceController {
    * @return the root discovery of outlook mail services
    */
   @RequestMapping(value = "", method = RequestMethod.GET, produces = OutlookConstant.HAL_AND_JSON)
-  public AbstractFileResource getRootDiscoveryOfOutlookMailServices() {
-    AbstractFileResource resource = null;
+  public ResourceSupport getRootDiscoveryOfOutlookExoServices() {
+    ResourceSupport resource = new ResourceSupport();
+
+    List<Link> links = new LinkedList<>();
+    links.add(linkTo(methodOn(RootDiscoveryeXoServiceController.class).getRootDiscoveryOfOutlookExoServices()).withSelfRel());
+    links.add(linkTo(methodOn(RootDiscoveryMailServiceController.class).getRootDiscoveryOfOutlookMailServices()).withRel("mailServices"));
+    links.add(linkTo(methodOn(UserController.class).getRoot()).withRel("userServices"));
+    links.add(linkTo(methodOn(SpaceController.class).getRoot()).withRel("spaceServices"));
+    links.add(linkTo(methodOn(ActivityController.class).getRoot()).withRel("activityServices"));
+    links.add(linkTo(methodOn(DocumentController.class).getRoot()).withRel("documentServices"));
+    resource.add(links);
 
     return resource;
   }
