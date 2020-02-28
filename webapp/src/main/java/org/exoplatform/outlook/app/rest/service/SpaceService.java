68b988e5606a93464c68bad9c10f9b36faa804cb
Nick Riabovol <nick.riabovol@gmail.com>
2020-02-28 16:43:33 +0200

    Added SpaceService

---
+++ webapp/src/main/java/org/exoplatform/outlook/app/rest/service/SpaceService.java
@@ -0,0 +1,53 @@
+package org.exoplatform.outlook.app.rest.service;
+
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.outlook.OutlookException;
+import org.exoplatform.outlook.OutlookService;
+import org.exoplatform.outlook.OutlookSpace;
+import org.exoplatform.outlook.OutlookSpaceException;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.jcr.RepositoryException;
+
+/**
+ * The type Space service.
+ */
+@Service
+public class SpaceService {
+
+  /** The Constant LOG. */
+  private static final Log LOG = ExoLogger.getLogger(SpaceService.class);
+
+  /**
+   * Gets outlook space.
+   *
+   * @param spaceId the space id
+   * @return the outlook space
+   */
+  public OutlookSpace getOutlookSpace(String spaceId) {
+    ExoContainer currentContainer = ExoContainerContext.getCurrentContainer();
+    OutlookService outlook = (OutlookService) currentContainer.getComponentInstance(OutlookService.class);
+
+    String spaceGroupId = new StringBuilder("/spaces/").append(spaceId).toString();
+
+    OutlookSpace outlookSpace = null;
+    try {
+      outlookSpace = outlook.getSpace(spaceGroupId);
+    } catch (OutlookSpaceException e) {
+      LOG.error("Error getting space (" + spaceGroupId + ")", e);
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error getting space (" + spaceGroupId + ")");
+    } catch (RepositoryException e) {
+      LOG.error("Error getting space (" + spaceGroupId + ")", e);
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error getting space (" + spaceGroupId + ")");
+    } catch (OutlookException e) {
+      LOG.error("Error getting space (" + spaceGroupId + ")", e);
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error getting space (" + spaceGroupId + ")");
+    }
+    return outlookSpace;
+  }
+}
