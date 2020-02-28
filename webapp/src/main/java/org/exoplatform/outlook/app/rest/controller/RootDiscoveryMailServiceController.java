d2db283c9d111f0f2c50740badd70ac4122f9c60
Nick Riabovol <nick.riabovol@gmail.com>
2020-02-29 01:48:09 +0200

    Implemented RootDiscoveryMailServiceController GET '/v2/mail' request handling

--- webapp/src/main/java/org/exoplatform/outlook/app/rest/controller/RootDiscoveryMailServiceController.java
+++ webapp/src/main/java/org/exoplatform/outlook/app/rest/controller/RootDiscoveryMailServiceController.java
@@ -1,11 +1,18 @@
 package org.exoplatform.outlook.app.rest.controller;
 
-import org.exoplatform.outlook.app.rest.dto.AbstractFileResource;
 import org.exoplatform.outlook.model.OutlookConstant;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.ResourceSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.springframework.hateoas.core.DummyInvocationUtils.methodOn;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 /**
  * The type Root discovery mail service controller.
@@ -21,8 +28,14 @@ public class RootDiscoveryMailServiceController {
    * @return the root discovery of outlook mail services
    */
   @RequestMapping(value = "", method = RequestMethod.GET, produces = OutlookConstant.HAL_AND_JSON)
-  public AbstractFileResource getRootDiscoveryOfOutlookMailServices() {
-    AbstractFileResource resource = null;
+  public ResourceSupport getRootDiscoveryOfOutlookMailServices() {
+    ResourceSupport resource = new ResourceSupport();
+
+    List<Link> links = new LinkedList<>();
+    links.add(linkTo(methodOn(RootDiscoveryMailServiceController.class).getRootDiscoveryOfOutlookMailServices()).withSelfRel());
+    links.add(linkTo(methodOn(RootDiscoveryeXoServiceController.class).getRootDiscoveryOfOutlookExoServices()).withRel("exoServices"));
+    links.add(linkTo(methodOn(MessageController.class).getRoot()).withRel("messageServices"));
+    resource.add(links);
 
     return resource;
   }
