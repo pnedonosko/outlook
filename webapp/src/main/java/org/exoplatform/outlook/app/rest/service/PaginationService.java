6a592c8cffd0ef908592719529417aa3c595f06d
Nick Riabovol <nick.riabovol@gmail.com>
2020-03-03 10:32:23 +0200

    Refactored PaginationService

--- webapp/src/main/java/org/exoplatform/outlook/app/rest/service/PaginationService.java
+++ webapp/src/main/java/org/exoplatform/outlook/app/rest/service/PaginationService.java
@@ -21,21 +21,21 @@ public class PaginationService {
    *
    * @param offset the offset
    * @param limit the limit
-   * @param activitiesNumber the activities number
+   * @param availableElementsNumber the available elements number
    * @return the pagination metadata
    */
-  public PagedResources.PageMetadata getPaginationMetadata(Integer offset, Integer limit, int activitiesNumber) {
+  public PagedResources.PageMetadata getPaginationMetadata(Integer offset, Integer limit, int availableElementsNumber) {
     int requestedDataSize = limit - offset;
     if (requestedDataSize <= 0) {
       LOG.error("Pagination error: offset - limit <= 0");
       throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Pagination error: offset - limit <= 0");
     }
 
-    long pages = (long) Math.ceil((double) activitiesNumber / requestedDataSize);
+    long pages = (long) Math.ceil((double) availableElementsNumber / requestedDataSize);
     int currentPostition = offset + 1;
     PagedResources.PageMetadata metadata = new PagedResources.PageMetadata(requestedDataSize,
                                                                            currentPostition,
-                                                                           activitiesNumber,
+                                                                           availableElementsNumber,
                                                                            pages);
     return metadata;
   }
